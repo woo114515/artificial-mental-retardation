b0713edb315dd3cfcd58c7e78a92a74f63972ef0
woo <woo114515@gmail.com>
2026-05-17 22:47:53 +0800

    Add: SGD optimizer

--- nn/optimizers.py
+++ nn/optimizers.py
@@ -1,4 +1,32 @@
-'''
+"""
 负责参数更新。
-目前只有SGD(Stochastic Gradient Descent)。
-'''
+目前只有 SGD (Stochastic Gradient Descent)。
+"""
+
+from __future__ import annotations
+
+
+class SGD:
+    def __init__(self, lr: float = 0.01):
+        """
+        lr: learning rate，学习率
+        """
+        self.lr = lr
+
+    def step(self, params_and_grads):
+        """
+        params_and_grads:
+            [
+                (param1, grad1),
+                (param2, grad2),
+                ...
+            ]
+
+        原地更新：
+            param = param - lr * grad
+        """
+        for param, grad in params_and_grads:
+            if grad is None:
+                continue
+
+            param[...] -= self.lr * grad
